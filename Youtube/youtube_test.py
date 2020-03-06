ff35485b3a3c4152e0abacc4f2688343e6749f2e
andywrussell <andrewwrussell1@outlook.com>
2020-03-06 11:52:26 +0000

    Started testing youtube

---
+++ Youtube/youtube_test.py
@@ -0,0 +1,7 @@
+#GOOGLE API Key AIzaSyAq4OIWZ19jvTC_2GFC3TdL0A9oCcFNc2Q
+
+from youtube_api import YouTubeDataAPI
+
+api_key = 'AIzaSyAq4OIWZ19jvTC_2GFC3TdL0A9oCcFNc2Q'
+yt = YouTubeDataAPI(api_key)
+
