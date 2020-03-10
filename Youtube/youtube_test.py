c149ba129b4d29eed63c228020d36d09d402a938
andywrussell <andrewwrussell1@outlook.com>
2020-03-10 14:46:29 +0000

    tests with yt -- limited by quota!

--- Youtube/youtube_test.py
+++ Youtube/youtube_test.py
@@ -2,8 +2,11 @@
 
 import pandas as pd
 from youtube_api import YouTubeDataAPI
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-api_key = 'AIzaSyAq4OIWZ19jvTC_2GFC3TdL0A9oCcFNc2Q'
+analyser = SentimentIntensityAnalyzer()
+
+api_key = 'AIzaSyA2Q_ASXYOtMaQj4outLXJUcoc5dCv_Zhs'
 yt = YouTubeDataAPI(api_key)
 
 sonic_search = yt.search(q = "Sonic The Headgehog")
@@ -15,6 +18,20 @@ df_sonic.head(5)
 trailer = df_sonic.iloc[0]
 trailer.video_id
 
-comments = yt.get_video_comments(trailer.video_id, max_results=100)
+comments = yt.get_video_comments(trailer.video_id, max_results=10)
 df_comments = pd.DataFrame(comments)
-df_comments.head(5)
+
+df_graph_data = pd.DataFrame(columns = ['comment_id', 'commenter_channel_id', 'channel_country', 'text', 'date', 'neg', 'neu', 'pos', 'compound'])
+
+channel_id = df_comments.iloc[0].commenter_channel_id
+channel_data = yt.get_channel_metadata(channel_id, part=['id', 'snippet', 'contentDetails'])
+
+# for index, row in df_comments.iterrows():
+#     channel_id = df_comments.iloc[0].commenter_channel_id
+#     channel_data = yt.get_channel_metadata(channel_id)
+    
+#     print
+    
+#     score = analyser.polarity_scores(row['text'])
+#     graph_row = {'comment_id': row['comment_id'], 'commenter_channel_id': row['commenter_channel_id'], 'channel_country' : channel_data['country'], 'text' : row['text'], 'date': row['collection_date'], 'neg': score['neg'], 'neu': score['neu'], 'pos': score['pos'], 'compound': score['compound']} 
+#     df_graph_data = df_graph_data.append(graph_row, ignore_index=True)
