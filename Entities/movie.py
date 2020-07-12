206f78e812bfb4a764e3233dea873b6420d2d2fa
andywrussell <andrewwrussell1@outlook.com>
2020-07-12 19:46:49 +0100

    heated time map on movies

--- Entities/movie.py
+++ Entities/movie.py
@@ -32,8 +32,7 @@ import numpy as np
 from colour import Color
 from datetime import datetime
 from datetime import timedelta
-
-
+import scipy.ndimage as ndi
 
 class Movie:
     def __init__(self, db_row):
@@ -403,8 +402,8 @@ class Movie:
         end_date = None
         
         if movie_run:    
-            start_date = datetime.combine((movie.ukReleaseDate - timedelta(days=14)), datetime.min.time())
-            end_date = datetime.combine((movie.first_run_end - timedelta(days=14)), datetime.min.time())
+            start_date = datetime.combine((self.ukReleaseDate - timedelta(days=14)), datetime.min.time())
+            end_date = datetime.combine((self.first_run_end - timedelta(days=14)), datetime.min.time())
 
          
         tweets = database_helper.select_geo_tweets(self.movieId, start_date, end_date)
@@ -458,7 +457,7 @@ class Movie:
         title = "{0} Time Map".format(self.title)
         
         if not (start_date == None) and (not end_date == None):
-            title += "{0} - {1}".format(start_date, end_date)
+            title += " {0} - {1}".format(start_date.date(), end_date.date())
             
         plt.title(title)
         plt.xticks(ticks,labels)
@@ -476,4 +475,103 @@ class Movie:
         
         plt.show()
         
+    def plot_heated_time_map(self, movie_run = False):
+        #adapted from https://districtdatalabs.silvrback.com/time-maps-visualizing-discrete-events-across-many-timescales        
+                
+        start_date = None
+        end_date = None
+        
+        if movie_run:    
+            start_date = datetime.combine((self.ukReleaseDate - timedelta(days=14)), datetime.min.time())
+            end_date = datetime.combine((self.first_run_end - timedelta(days=14)), datetime.min.time())
+
+         
+        tweets = database_helper.select_geo_tweets(self.movieId, start_date, end_date)
+        tweets = tweets.sort_values(by=['created_at']).reset_index()
+        times = tweets['created_at']
+        times_tot_mins = 24*60 - (60*np.array([t.hour for t in times]) + np.array([t.minute for t in times]))
+        
+        seps=np.array([(times[i]-times[i-1]).total_seconds() for i in range(1,len(times))])
+        seps[seps==0]=1 # convert zero second separations to 1-second separations
+        
+        sep_array=np.zeros((len(seps)-1,2)) # 1st column: x-coords, 2nd column: y-coords
+        sep_array[:,0]=seps[:-1]
+        sep_array[:,1]=seps[1:]
+        
+        Nside=4*256 # number of pixels along the x and y directions
+        width=4 # the number of pixels that specifies the width of the Gaussians for the Gaussian filter
+        
+        # choose points within specified range. Example plot separations greater than 5 minutes:
+        #	indices = (sep_array[:,0]>5*60) & (sep_array[:,1]>5*60)
+        indices=range(sep_array.shape[0]) # all time separations
+        
+        x_pts = np.log(sep_array[indices,0])
+        y_pts = np.log(sep_array[indices,1])
+        
+        
+        #x_pts = sep_array[indices,0]
+        #y_pts = sep_array[indices,1]
+        
+        min_val = np.min([np.min(x_pts), np.min(y_pts)])
+           	
+        x_pts = x_pts - min_val
+        y_pts = y_pts - min_val
+           	
+        max_val = np.max([np.max(x_pts), np.max(y_pts)])
+           	
+        x_pts = x_pts * (Nside-1)/max_val
+        y_pts = y_pts * (Nside-1)/max_val
+           	
+        img=np.zeros((Nside,Nside))
+           
+        for i in range(len(x_pts)):
+            img[int(x_pts[i]),int(y_pts[i])] +=1
+           
+        img = ndi.gaussian_filter(img,width) # apply Gaussian filter
+        img = np.sqrt(img) # taking the square root makes the lower values more visible
+        img=np.transpose(img) # needed so the orientation is the same as scatterplot
+           
+        plt.imshow(img, origin='lower')
+           	
+        ## create custom tick marks. Calculate positions of tick marks on the transformed log scale of the image array
+        plt.minorticks_off()
+           	
+        ## change font, which can also now accept latex: http://matplotlib.org/users/usetex.html
+        plt.rc('text',usetex=False)
+        plt.rc('font',family='serif')
+           
+        my_max = np.max([np.max(sep_array[indices,0]), np.max(sep_array[indices,1])])
+        my_min = np.max([np.min(sep_array[indices,0]), np.min(sep_array[indices,1])])
+        
+        pure_ticks = np.array([1e-3,1,10,60*10,2*3600,1*24*3600, 7*24*3600]) 
+        # where the tick marks will be placed, in units of seconds. An additional value will be appended to the end for the max
+        labels = ['1 msec','1 sec','10 sec','10 min','2 hr','1 day','1 week']  # tick labels
+           
+        index_lower=np.min(np.nonzero(pure_ticks >= my_min)) 
+        # index of minimum tick that is greater than or equal to the smallest time interval. This will be the first tick with a non-blank label
+           
+        index_upper=np.max(np.nonzero(pure_ticks <= my_max))
+        # similar to index_lower, but for upperbound
+           	
+        ticks = pure_ticks[index_lower: index_upper + 1]
+        ticks = np.log(np.hstack((my_min, ticks, my_max ))) # append values to beginning and end in order to specify the limits
+        ticks = ticks - min_val
+        ticks *= (Nside-1)/(max_val)
+           	
+        labels= np.hstack(('',labels[index_lower:index_upper + 1],'')) # append blank labels to beginning and end
+        
+        title = "{0} Time Map".format(self.title)
+        
+        if not (start_date == None) and (not end_date == None):
+            title += " {0} - {1}".format(start_date.date(), end_date.date())
+            
+        plt.title(title)
+        plt.xticks(ticks,labels)
+        plt.yticks(ticks,labels)
+        plt.xticks(rotation=40)
+         	
+        plt.xlabel('Time Before Tweet')
+        plt.ylabel('Time After Tweet')
+        plt.show()
+
         
--- Visualizations/timemap_test.py
+++ Visualizations/timemap_test.py
@@ -31,6 +31,7 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from nameparser import HumanName
 from datetime import datetime
 from datetime import timedelta
+import scipy.ndimage as ndi
 
 movie = movie_helper.get_movie_by_id(28)
 
@@ -47,60 +48,81 @@ times_tot_mins = 24*60 - (60*np.array([t.hour for t in times]) + np.array([t.min
 seps=np.array([(times[i]-times[i-1]).total_seconds() for i in range(1,len(times))])
 seps[seps==0]=1 # convert zero second separations to 1-second separations
 
+seps = seps.astype(int)
+
 sep_array=np.zeros((len(seps)-1,2)) # 1st column: x-coords, 2nd column: y-coords
 sep_array[:,0]=seps[:-1]
 sep_array[:,1]=seps[1:]
 
+sep_array = sep_array.astype(int)
+
 Ncolors=24*60 
 
-## set up color list
-red=Color("red")
-blue=Color("blue")
-color_list = list(red.range_to(blue, Ncolors)) # range of colors evenly speced on the spectrum between red and blue. Each element is a colour object
-color_list = [c.hex for c in color_list] # give hex version
+Nside=4*256 # number of pixels along the x and y directions
+width=4 # the number of pixels that specifies the width of the Gaussians for the Gaussian filter
 
-fig=plt.figure()
-ax =fig.add_subplot(111)
+# choose points within specified range. Example plot separations greater than 5 minutes:
+#	indices = (sep_array[:,0]>5*60) & (sep_array[:,1]>5*60)
+indices=range(sep_array.shape[0]) # all time separations
 
+x_pts = np.log(sep_array[indices,0])
+y_pts = np.log(sep_array[indices,1])
+
+
+
+#x_pts = sep_array[indices,0]
+#y_pts = sep_array[indices,1]
+
+min_val = np.min([np.min(x_pts), np.min(y_pts)])
+   	
+x_pts = x_pts - min_val
+y_pts = y_pts - min_val
+   	
+max_val = np.max([np.max(x_pts), np.max(y_pts)])
+   	
+x_pts = x_pts * (Nside-1)/max_val
+y_pts = y_pts * (Nside-1)/max_val
+   	
+img=np.zeros((Nside,Nside))
+   
+for i in range(len(x_pts)):
+    img[int(x_pts[i]),int(y_pts[i])] +=1
+   
+img = ndi.gaussian_filter(img,width) # apply Gaussian filter
+img = np.sqrt(img) # taking the square root makes the lower values more visible
+img=np.transpose(img) # needed so the orientation is the same as scatterplot
+   
+plt.imshow(img, origin='lower')
+   	
+## create custom tick marks. Calculate positions of tick marks on the transformed log scale of the image array
+plt.minorticks_off()
+   	
+## change font, which can also now accept latex: http://matplotlib.org/users/usetex.html
 plt.rc('text',usetex=False)
 plt.rc('font',family='serif')
- 	
-colormap = plt.cm.get_cmap('rainbow')  # see color maps at http://matplotlib.org/users/colormaps.html
+   
+my_max = np.max([np.max(sep_array[indices,0]), np.max(sep_array[indices,1])])
+my_min = np.max([np.min(sep_array[indices,0]), np.min(sep_array[indices,1])])
 
-order=np.argsort(times_tot_mins[1:-1]) # so that the red dots are on top
-#	order=np.arange(1,len(times_tot_mins)-2) # dots are unsorted
-
-sc= ax.scatter(sep_array[:,0][order],sep_array[:,1][order],c=times_tot_mins[1:-1][order],vmin=0,vmax=24*60,s=25,cmap=colormap,marker='o',edgecolors='none')
-# taken from http://stackoverflow.com/questions/6063876/matplotlib-colorbar-for-scatter
- 	
-color_bar=fig.colorbar(sc,ticks=[0,24*15,24*30,24*45,24*60],orientation='horizontal',shrink=0.5)
-color_bar.ax.set_xticklabels(['Midnight','18:00','Noon','6:00','Midnight'])
-color_bar.ax.invert_xaxis()
-color_bar.ax.tick_params(labelsize=16)
- 	
-ax.set_yscale('log') # logarithmic axes
-ax.set_xscale('log')
-
-plt.minorticks_off()
-pure_ticks = np.array([1e-3,1,10,60*10,2*3600,1*24*3600, 7*24*3600]) # where the tick marks will be placed, in units of seconds.
+pure_ticks = np.array([1e-3,1,10,60*10,2*3600,1*24*3600, 7*24*3600]) 
+# where the tick marks will be placed, in units of seconds. An additional value will be appended to the end for the max
 labels = ['1 msec','1 sec','10 sec','10 min','2 hr','1 day','1 week']  # tick labels
-
-max_val = np.max([np.max(sep_array[:,0]), np.max(sep_array[:,1])])
- 	
-ticks = np.hstack((pure_ticks, max_val))
-
-min_val = np.min([np.min(sep_array[:,0]), np.min(sep_array[:,1])])
- 	
-plt.xticks(ticks,labels,fontsize=16)
-plt.yticks(ticks,labels,fontsize=16)
- 	
+   
+index_lower=np.min(np.nonzero(pure_ticks >= my_min)) 
+# index of minimum tick that is greater than or equal to the smallest time interval. This will be the first tick with a non-blank label
+   
+index_upper=np.max(np.nonzero(pure_ticks <= my_max))
+# similar to index_lower, but for upperbound
+   	
+ticks = pure_ticks[index_lower: index_upper + 1]
+ticks = np.log(np.hstack((my_min, ticks, my_max ))) # append values to beginning and end in order to specify the limits
+ticks = ticks - min_val
+ticks *= (Nside-1)/(max_val)
+   	
+labels= np.hstack(('',labels[index_lower:index_upper + 1],'')) # append blank labels to beginning and end
+plt.xticks(ticks, labels,fontsize=16)
+plt.yticks(ticks, labels,fontsize=16)
 plt.xlabel('Time Before Tweet',fontsize=18)
-plt.ylabel('Time After Tweet',fontsize=18)
-
-plt.xlim((min_val, max_val))
-plt.ylim((min_val, max_val))
- 	
-ax.set_aspect('equal')
-plt.tight_layout()
-
+plt.ylabel('Time After Tweet' ,fontsize=18)
 plt.show()
+ 	
